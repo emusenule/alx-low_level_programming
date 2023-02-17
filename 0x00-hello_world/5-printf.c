dd004aa787522122c15e2fe69af064f6331c691d
emusenule <emusenjuleww@gmail.com>
2023-02-17 11:34:17 -0800

    Print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
+ * main - A program that prints aline with puts function
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
