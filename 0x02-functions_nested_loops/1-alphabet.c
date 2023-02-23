b4261112ce99cd067a94ef5ad5185a79b92ac17c
emusenule <emusenjuleww@gmail.com>
2023-02-22 17:23:43 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that print alphabet in lower case
+ *
+ * Return: always 0;
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar((char) i);
+	}
+		_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef FNL
+#define FNL
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -18,8 +20,10 @@ void jack_bauer(void);
 
 void times_table(void);
 
+
 int add(int, int);
 
 void print_to_98(int n);
 
 void print_times_table(int n);
+#endif
