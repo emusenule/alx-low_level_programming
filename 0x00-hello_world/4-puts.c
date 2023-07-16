d51b49fcfc6f230996fc69bb4ed644f08507dd22
emusenule <emusenjuleww@gmail.com>
2023-07-16 03:03:10 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descriptoion: prints "Programming like building apolyglot puzzle"
+ *
+ * Retuern: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a polyglot puzzle");
+	return (0);
+}
