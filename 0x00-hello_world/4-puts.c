1889e57a8e8722aaf551719c5b261f99988b299f
emusenule <emusenjuleww@gmail.com>
2023-07-16 03:34:58 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Descriptoion: prints "Programming like building apolyglot puzzle"
+ * Descriptoion: prints "Programming like building a multilingual puzzle"
  *
  * Retuern: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"programming is like building a polyglot puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
