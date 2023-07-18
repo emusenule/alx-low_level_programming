3184709694c5705040675ad6ab7ebae7967496f7
emusenule <emusenjuleww@gmail.com>
2023-07-18 07:53:26 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putcher, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putcher('_');
+	_putcher('p');
+	_putcher('u');
+	_putcher('t');
+	_putcher('c');
+	_putcher('h');
+	_putcher('e');
+	_putcher('r');
+	_putcher('\n');
+
+	return (0);
+}
