e62a22437c66a18d5feaf87f6124cf2aa46ea81f
emusenule <emusenjuleww@gmail.com>
2023-07-25 13:13:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
- * main - prints _putcher, followed by a new line
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
 int main(void)
 {
-	_putcher('_');
-	_putcher('p');
-	_putcher('u');
-	_putcher('t');
-	_putcher('c');
-	_putcher('h');
-	_putcher('e');
-	_putcher('r');
-	_putcher('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
