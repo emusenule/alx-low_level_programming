0187fcf5a36dc8aa4a7a9f1486940dd3d654b0e9
emusenule <emusenjuleww@gmail.com>
2023-12-29 07:36:31 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,7 +8,7 @@
  * @n: value of the element
  * Return: the address of the new element
  */
-distint_t *add_dnodeint (dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 	dlistint_t *h;
@@ -17,22 +17,22 @@ distint_t *add_dnodeint (dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-		new->n = n;
-		new->prev = NULL;
-		h = *head;
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
 
-		if (h != NULL)
-		{
-			while (h->prev != NULL)
-				h = h->prev;
-		}
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
 
-		new->next = h;
+	new->next = h;
 
-		if (h != NULL)
-			h->prev = new;
+	if (h != NULL)
+		h->prev = new;
 
-		*head = new;
+	*head = new;
 
-		return (new);
+	return (new);
 }
